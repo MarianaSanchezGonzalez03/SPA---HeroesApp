e3814434daf8400cac1210874628904fcb80773c
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2022-03-20 23:11:03 -0600

    Creando un primer Router

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -2,8 +2,6 @@
 
 export const HeroesApp = () => {
     return (
-      <div>
-          <h1>Heroes App</h1>
-      </div>
+        <AppRouter />   
     )
   }
---
+++ src/componentes/search/SearchScreen.js
@@ -0,0 +1,7 @@
+export const SerchScreen = () => {
+    return (
+      <div>
+          <h1>SearchScrren</h1>
+      </div>
+    )
+  }
